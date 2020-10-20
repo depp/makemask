ad407cf05050cecf63ff7b0bb6d8986be9bd4dae
Dietrich Epp <depp@zdome.net>
2020-10-20 05:00:26 -0400

    Clean up code, add error handling

--- crc.go
+++ crc.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/binary"
 	"errors"
+	"io"
 	"os"
 )
 
-const CHECKSUM_START = 0x00001000
-const CHECKSUM_LENGTH = 0x00100000
+const (
+	checksumStart  = 0x00001000
+	checksumLength = 0x00100000
+)
 
 // This file is mostly copied wholesale from http://n64dev.org/n64crc.html.
 
@@ -15,8 +18,8 @@ func rol(num uint32, shift uint32) uint32 {
 	return (num << shift) | (num >> (32 - shift))
 }
 
-func CalculateN64Crc(f *os.File, bootcode int) ([8]byte, error) {
-	var seed uint32 = 0xF8CA4DDC
+func calculateN64Crc(f *os.File, bootcode int) (crc [8]byte, err error) {
+	const seed uint32 = 0xF8CA4DDC
 	t1 := seed
 	t2 := seed
 	t3 := seed
@@ -24,19 +27,19 @@ func CalculateN64Crc(f *os.File, bootcode int) ([8]byte, error) {
 	t5 := seed
 	t6 := seed
 
-	crc := [8]byte{}
 	crcInts := [2]uint32{}
 	if bootcode != 6102 {
-		return crc, errors.New("Non-6102 bootcodes not yet supported.")
+		return crc, errors.New("non-6102 bootcodes not yet supported")
 	}
-	data := make([]byte, CHECKSUM_LENGTH)
-	_, err := f.ReadAt(data, CHECKSUM_START)
-	if err != nil {
+	data := make([]byte, checksumLength)
+	if _, err := f.ReadAt(data, checksumStart); err != nil {
+		if err == io.EOF {
+			return crc, errors.New("ROM too short")
+		}
 		return crc, err
 	}
 
-	i := 0
-	for i < CHECKSUM_LENGTH {
+	for i := 0; i < checksumLength; i += 4 {
 		d := binary.BigEndian.Uint32(data[i:])
 		if (t6 + d) < t6 {
 			t4++
@@ -57,8 +60,6 @@ func CalculateN64Crc(f *os.File, bootcode int) ([8]byte, error) {
 		} else {
 			t1 += t5 ^ d
 		}
-
-		i += 4
 	}
 	if bootcode == 6103 {
 		crcInts[0] = (t6 ^ t4) + t3
--- main.go
+++ main.go
@@ -6,35 +6,50 @@ import (
 	"os"
 )
 
-func main() {
+func makeMask(filename string, bootdata []byte) error {
+	fmt.Printf("Masking %s.\n", filename)
+	f, err := os.OpenFile(filename, os.O_RDWR, 0755)
+	if err != nil {
+		return fmt.Errorf("could not open ROM: %v", err)
+	}
+	defer f.Close()
+	_, err = f.WriteAt(bootdata, 0x40)
+	if err != nil {
+		return fmt.Errorf("could not write: %v", err)
+	}
+	crcBytes, err := calculateN64Crc(f, 6102)
+	if err != nil {
+		return fmt.Errorf("could not calculate CRC: %v", err)
+	}
+	fmt.Printf("Generate checksum: 0x%X\n", crcBytes)
+	_, err = f.WriteAt(crcBytes[:], 0x10)
+	if err != nil {
+		return fmt.Errorf("could not write: %v", err)
+	}
+	return nil
+}
+
+func mainE() error {
 	flag.Parse()
 	if len(flag.Args()) == 0 {
-		fmt.Println("Usage: makemask [rom file, rom file,...]")
+		fmt.Fprintln(os.Stderr, "Usage: makemask [rom file, rom file, ...]")
+		os.Exit(64)
 	}
 	bootdata, err := Asset("data/boot.6102")
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("could not load boot data: %v", err)
 	}
-	for arg := range flag.Args() {
-		name := flag.Arg(arg)
-		fmt.Printf("Masking %s.\n", name)
-		f, err := os.OpenFile(name, os.O_RDWR, 0755)
-		if err != nil {
-			panic(err)
+	for _, arg := range flag.Args() {
+		if err := makeMask(arg, bootdata); err != nil {
+			return err
 		}
-		defer f.Close()
-		_, err = f.WriteAt(bootdata, 0x40)
-		if err != nil {
-			panic(err)
-		}
-		crcBytes, err := CalculateN64Crc(f, 6102)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Generate checksum: 0x%X\n", crcBytes)
-		_, err = f.WriteAt(crcBytes[:], 0x10)
-		if err != nil {
-			panic(err)
-		}
+	}
+	return nil
+}
+
+func main() {
+	if err := mainE(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
 	}
 }
